d0b3d44e112f129e1d7aba3282dd11437bea86d0
namcoindev <69799859+namcoindev@users.noreply.github.com>
2020-09-07 15:42:52 +0200

    Update config.js

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,5 +1,7 @@
 const ExplorerConfig = {
   apiBaseUrl: 'https://api.namcoin.org',
+  poolApiUrl: 'https://poolparty.namcoin.org/',
+  poolListUrl: 'https://github.com/namcoindev/pool-list/blob/master/pools.json',
   ticker: 'NAM',
   maxSupply: 10000000000000,
   emissionSpeed: 21,
